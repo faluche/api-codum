2fd8c0ffd104250e245893aa81895ae0c65e1cab
Sofiane ZOUAOUI <sofiane050900@gmail.com>
2021-08-29 19:09:07 +0200

    init commit

---
+++ src/test/java/fr/faluche/codum/ApiCodumApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.faluche.codum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiCodumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
